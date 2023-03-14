470a81fc793efe372bb65af98f989bac4144977f
Danush <64687472+danushv07@users.noreply.github.com>
2023-03-14 17:38:14 +0100

    Update eval_ace.py
    added report function

--- src/eval_ace.py
+++ src/eval_ace.py
@@ -1,3 +1,54 @@
-metadata_test=example_metadata.loc[test_index,:]
-ace_metadata_test=metadata_test.loc[metadata_test['dataset']=='Ace_20'].reset_index(drop = True)
-mat_metadata_test=metadata_test.loc[metadata_test['dataset']=='Mat_19'].reset_index(drop = True)
+"""
+This files creates the submission file for the challenge
+"""
+import os
+from time import sleep
+from glob import glob
+import random
+from tqdm import tqdm
+import copy
+import ntpath
+
+import numpy as np
+from imageio import imread
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, plot_confusion_matrix, matthews_corrcoef, classification_report,confusion_matrix, accuracy_score, balanced_accuracy_score, cohen_kappa_score, f1_score,  precision_score, recall_score
+
+import torch
+import torch.nn as nn
+import torch.optim
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import transforms
+import torchvision.models as models
+from torchvision.models import resnet18
+import click
+
+from models import *
+from data_load import *
+
+def classification_complete_report(y_true, y_pred ,labels = None , name='report.png'): 
+    print(classification_report(y_true, y_pred, labels = None))
+    print(15*"----")
+    print("matthews correlation coeff: %.4f" % (matthews_corrcoef(y_true, y_pred)) )
+    print("Cohen Kappa score: %.4f" % (cohen_kappa_score(y_true, y_pred)) )
+    print("Accuracy: %.4f & balanced Accuracy: %.4f" % (accuracy_score(y_true, y_pred), balanced_accuracy_score(y_true, y_pred)) )
+    #print("macro F1 score: %.4f & micro F1 score: %.4f" % (f1_score(y_true, y_pred, average = "macro"), f1_score(y_true, y_pred, average = "micro")) )
+    print("macro Precision score: %.4f & micro Precision score: %.4f" % (precision_score(y_true, y_pred, average = "macro"), precision_score(y_true, y_pred, average = "micro")) )
+    print("macro Recall score: %.4f & micro Recall score: %.4f" % (recall_score(y_true, y_pred, average = "macro"), recall_score(y_true, y_pred, average = "micro")) )
+    print(labels)
+    cm = confusion_matrix(y_true, y_pred,labels= labels, normalize='true')
+    fig, ax = plt.subplots(figsize=(10, 10)) #plot size
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap=plt.cm.Blues, xticks_rotation='vertical', ax=ax, include_values=False, colorbar=False)
+    
+    #plt.show()
+    plt.savefig(name, dpi=500)
+    print(15*"----")
